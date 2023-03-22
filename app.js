03a3428c3e14a053c4c5625b5ab786d6441d3df4
Gerardo Delao <Gerardo_delao@ymail.com>
2023-03-21 22:35:08 -0600

    updated changes

---
+++ app.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      showModal: false,
+    };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true;
+    },
+    closeModal() {
+      this.showModal = false;
+    },
+  },
+});
+app.mount("#app");
